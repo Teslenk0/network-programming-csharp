1c5c143554e082b543a8cffbf8c74216d62c018c
Brahian Pena <teslasapbe@gmail.com>
2022-09-17 18:17:15 -0300

    feat: added example for sockets

---
+++ EjemploSocket/Cliente/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Iniciando cliente");
+
+var socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+socketClient.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000));
+
+Console.WriteLine("Cliente conectado");
+
+
+while (true)
+{
+    Console.WriteLine("Escribe un mensaje");
+    
+    string mensaje = Console.ReadLine();
+
+    if(mensaje.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+    {
+        break;
+    }
+
+    byte[] data = Encoding.UTF8.GetBytes(mensaje);
+
+    socketClient.Send(data);
+}
+
+Console.ReadLine();
+
+socketClient.Shutdown(SocketShutdown.Both);
+
+socketClient.Close();
---
+++ EjemploSocket/EjemploSocket/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+
+    private const int MAX_CLIENTS = 2;
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Arrancando servidor");
+
+        var socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        var localEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+
+        socketServer.Bind(localEndpoint);
+
+        socketServer.Listen(1);
+
+        int counter = 0;
+
+        while (MAX_CLIENTS > counter)
+        {
+            var socketClient = socketServer.Accept();
+
+            new Thread(() => ManejarCliente(socketClient)).Start();
+        }
+
+        Console.ReadLine();
+
+        socketServer.Shutdown(SocketShutdown.Both);
+
+        socketServer.Close();
+    }
+
+    public static void ManejarCliente(Socket socketCliente)
+    {
+        
+
+        try
+        {
+            Console.WriteLine("Se acepto una nueva conexion");
+
+            while (socketCliente.Connected)
+            {
+                byte[] data = new byte[1048576];
+
+                socketCliente.Receive(data);
+
+                var parsedData = Encoding.UTF8.GetString(data);
+
+                Console.WriteLine(parsedData);
+
+                socketCliente.Send(data);
+            }
+
+            Console.WriteLine("Cliente desconectado");
+        }
+        catch(SocketException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        
+    }
+}
